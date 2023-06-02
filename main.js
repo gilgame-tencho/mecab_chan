588e4f30fd4095f84ddf7455d6e6bb22a63e031e
gilgame-tencho <tokubetugosetuchitai.ttiw@gmail.com>
2023-06-02 09:29:08 +0900

    input is file.

--- main.js
+++ main.js
@@ -8,13 +8,13 @@ const server_conf = yaml.parse(fs.readFileSync(__dirname + '/conf/server_conf.ym
 
 const kuromoji = require("kuromoji");
 
-let word = "親譲おやゆずりの無鉄砲むてっぽうで小供の時から損ばかりしている。小学校に居る時分学校の二階から飛び降りて一週間ほど腰こしを抜ぬかした事がある。なぜそんな無闇むやみをしたと聞く人があるかも知れぬ。別段深い理由でもない。新築の二階から首を出していたら、同級生の一人が冗談じょうだんに、いくら威張いばっても、そこから飛び降りる事は出来まい。弱虫やーい。と囃はやしたからである。小使こづかいに負ぶさって帰って来た時、おやじが大きな眼めをして二階ぐらいから飛び降りて腰を抜かす奴やつがあるかと云いったから、この次は抜かさずに飛んで見せますと答えた。";
+let words = fs.readFileSync(__dirname + '/data/bochan.txt', 'utf-8');
 let kts_words;
 let summay = { sum: 0 };
 
 kuromoji.builder({ dicPath: "node_modules/kuromoji/dict" }).build(function (err, tokenizer) {
   // tokenizer is ready
-  kts_words = tokenizer.tokenize("親譲おやゆずりの無鉄砲むてっぽうで小供の時から損ばかりしている。小学校に居る時分学校の二階から飛び降りて一週間ほど腰こしを抜ぬかした事がある。なぜそんな無闇むやみをしたと聞く人があるかも知れぬ。別段深い理由でもない。新築の二階から首を出していたら、同級生の一人が冗談じょうだんに、いくら威張いばっても、そこから飛び降りる事は出来まい。弱虫やーい。と囃はやしたからである。小使こづかいに負ぶさって帰って来た時、おやじが大きな眼めをして二階ぐらいから飛び降りて腰を抜かす奴やつがあるかと云いったから、この次は抜かさずに飛んで見せますと答えた。");
+  kts_words = tokenizer.tokenize(words);
   kts_words.forEach((word)=>{
     if(summay[word.surface_form]){
         summay[word.surface_form]++;
@@ -29,4 +29,5 @@ kuromoji.builder({ dicPath: "node_modules/kuromoji/dict" }).build(function (err,
 
 setTimeout(()=>{
     console.log(summay);
+    console.log(summay.sum);
 },2000);
